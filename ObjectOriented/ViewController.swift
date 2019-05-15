a4b9fb853e2486a066fa3cf32e1b3eb7a6171126
Ricky Erdiansyah <erdiansyah.ricky@gmail.com>
2019-05-15 09:23:53 +0700

    Initial Commit

---
+++ ObjectOriented/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ObjectOriented
+//
+//  Created by Ricky Erdiansyah on 15/05/19.
+//  Copyright © 2019 Ricky Erdiansyah. All rights reserved.
+//
+
+import UIKit
+
+class meja{
+    var warna: String
+    var kaki: Int
+    var bahanMeja: String
+    
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
